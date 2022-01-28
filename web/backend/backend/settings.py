3aa0ecaacfc365cf070985eaec003b92766f7f5d
reinattwijaya <71814354+reinattwijaya@users.noreply.github.com>
2022-01-28 15:35:20 +0900

    add search api requirements

--- web/backend/backend/settings.py
+++ web/backend/backend/settings.py
@@ -140,5 +140,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
